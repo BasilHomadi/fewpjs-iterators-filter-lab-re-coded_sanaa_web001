2f3370458b356778e8ecab1aa713a0a2f172c6ca
Basil Homadi <deviltearbasil@gmail.com>
2020-09-18 13:11:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+let drivers= ['Jack', 'John', 'Sam', 'Tailor', 'Tom', 'Albert'];
+
+function findMatching(x,name){
+    let result= x.filter (index=>
+        index.toLowerCase()===name.toLowerCase()
+    )
+    return result;
+}
+console.log(findMatching(drivers,'sam'));
